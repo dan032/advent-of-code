36d3f41c6db1064b6205fca8781d44defd10f04b
Daniel Butts <buttsda@sheridancollege.ca>
2023-12-14 17:58:36 -0500

    2023 14 p2

--- 2023/day14/main.rb
+++ 2023/day14/main.rb
@@ -1,63 +1,89 @@
 class Solution
-  attr_reader :data
+  attr_reader :data, :directions
+  ITERATIONS = 1_000_000_000
 
   def initialize
-    @data = File.read("#{Dir.pwd}/2023/day14/input.txt", chomp: true)
+    @data = File.read("#{Dir.pwd}/2023/day14/input.txt", chomp: true).split("\n").map{_1.split("")}
   end
 
   def problem1
-    result = 0
-    data.split("\n").map{_1.split("")}.transpose.each_with_index do |line, l_idx|
-      line = line.reverse
-      curr = 0
-      last_idx = 0
-      line.each_with_index do |c, i|
-        if c == "#"
-          if curr == 0
-            last_idx = i
-            next
-          end
-
-          (last_idx..i - 1).each do |new_idx|
-            next if line[new_idx] == '#'
-            if (i - new_idx) > curr
-              line[new_idx] = "."
-            else
-              line[new_idx] = 'O'
-            end
-          end
-          curr = 0
-          last_idx = i
-        elsif c == "O"
-          curr += 1
-        end
-      end
-      (last_idx..line.size - 1).each do |new_idx|
-        next if line[new_idx] == '#'
-        if line.size - new_idx > curr
-          line[new_idx] = "."
-        else
-          line[new_idx] = 'O'
-        end
-      end
-
-      result += get_result(line)
-    end
-    p result
+    grid = roll(data, "north")
+    p grid.transpose.each.reduce(0) { |result, line| result + get_line_result(line) }
   end
 
   def problem2
-    # p data
+    known_states = {}
+    count = 0
+    grid = data.dup
+
+    while count < ITERATIONS
+      %w[north west south east].each { grid = roll(grid, _1) }
+      if known_states.key?(grid.hash)
+        length = count - known_states[grid.hash]
+        skips = (ITERATIONS - count) / length
+        count += skips * length
+      else
+        known_states[grid.hash] = count
+      end
+      count += 1
+    end
+
+    p grid.transpose.each.reduce(0) { |result, line| result + get_line_result(line) }
   end
 
   private
 
-  def get_result(line)
-    res = 0
-    line.each_with_index do |c, i|
-      res += 1 * (i + 1) if c == "O"
+  def roll(grid, dir)
+    case dir
+    when "north"
+      grid.reverse.transpose.map { new_line(_1, &method(:fill)) }.transpose.reverse
+    when "west"
+      grid.map { new_line(_1, &method(:fill_west)) }
+    when "south"
+      grid.transpose.map { new_line(_1, &method(:fill)) }.transpose
+    when "east"
+      grid.reverse.map { new_line(_1, &method(:fill)) }.reverse
+    else
+      raise StandardError.new("Unsupported Direction")
     end
-    res
+  end
+
+  def get_line_result(line)
+    line.each_with_index.reduce(0) { |res, char_with_idx| char_with_idx[0] == "O" ? res + 1 * (line.size - char_with_idx[1]) : res }
+  end
+
+  def new_line(line, &fill_func)
+    count = 0
+    last_index = 0
+    line.each_with_index do |c, i|
+      next count += 1 if c == 'O'
+      next unless c == '#'
+      next last_index = i if count == 0
+
+      line = fill_func.call(last_index, i, line, count)
+      count = 0
+      last_index = i
+    end
+    fill_func.call(last_index, line.size, line, count)
+  end
+
+  def fill(start_index, last_index, line, count)
+    (start_index..last_index - 1).each do |idx|
+      next if line[idx] == '#'
+      next line[idx] = 'O' unless (last_index - idx) > count
+      line[idx] = '.'
+    end
+    line
+  end
+
+  def fill_west(start_index, last_index, line, count)
+    (start_index..last_index - 1).each do |idx|
+      next if line[idx] == '#'
+      next line[idx] = '.' unless count > 0
+      line[idx] = 'O'
+      count -= 1
+    end
+    line
   end
 end
 
